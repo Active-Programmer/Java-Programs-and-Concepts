6b5e08c20b93920c051db493e18fb823b81e7a47
Active-Programmer <69895419+Active-Programmer@users.noreply.github.com>
2021-04-17 12:29:10 +0530

    Add files via upload
    WAP TO PRINT MULTIPLICATION TABLE OF 20x20..

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -16,10 +16,17 @@ public class MultiplicationTable {
 		byte number = reader.nextByte();
 		
 		System.out.println("Table of "+ number + " is -");
-		for(int i=1; i<=10; i++) {
-			
-			System.out.println(number*i);
+		
+		if(number == 0) {
+			System.out.println(0);
 		}
+		else {
+			for(int i=1; i<=10; i++) {
+				
+				System.out.println(number*i);
+			}
+		}
+		
 
 	}
 
