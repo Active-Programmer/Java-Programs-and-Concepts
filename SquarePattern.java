83bfb56d3f3c3f00a26d88031b571ecb045e75fe
Active-Programmer <69895419+Active-Programmer@users.noreply.github.com>
2021-04-17 12:49:42 +0530

    Add files via upload
    *  *  *  *
     *  *  *  *
     *  *  *  *
     *  *  *  *

---
+++ SquarePattern.java
@@ -0,0 +1,26 @@
+
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+package loops;
+
+public class SquarePattern {
+
+	public static void main(String[] args) {
+		
+		for(int j=1; j<=4; j++) {
+			
+			for(int i=1; i<=4; i++) {
+				System.out.print(" * ");
+			}
+			
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
